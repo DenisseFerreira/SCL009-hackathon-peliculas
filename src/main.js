52a126910a2f5883f4df4d443bad01d025d11943
DenisseFerreira <ferreira.denisse@gmail.com>
2019-05-23 00:35:44 -0400

    archivos duplicados borrados, orden en html y main

--- src/main.js
+++ src/main.js
@@ -1,34 +1,3 @@
-// /*Declarar variables globales*/
-// const urlOmdb = "http://www.omdbapi.com/?apikey=9a3646ed&type=movie";
-
-
-
-
-// /*Utilización de fetch*/
-// fetch (urlOmdb)
-//  .then((success) => { success.json() ; console.log(success.json()); } )
-//  .then((movies) => {console.log(urlOmdb);  console.log(Object.values(movies)) } )
-//  .catch((error)=>{ console.log(error)});
-
-
-/* --------------------------------------------------- */
-/* Tareas a realizar */
-/* 1 buscar urls */
-/* 2 obtener data de url */
-/* 3 mostrar en html */
-/* ----------------------------------------------------- */
-
-// require('es6-promise').polyfill();
-// require('isomorphic-fetch');
-/* 1 buscar url  inicio */
-/*  Traer data de un id se usa este 
-http://www.omdbapi.com/?apikey=4896bf68&i=tt3062350
-
-Obtener todos los datos de las ID de marvel que nosotros les pasemos
-El boton buscar sera solo con nuestras peliculas marvel
-*/
-/*  fin buscar url */
-
 //  Valores del DOM
 const contenedorPeliculas2 = document.getElementById('contenedorPeliculas2'); 
 const indicadorCarrusel = document.getElementById('indicadorCarrusel'); 
@@ -72,8 +41,7 @@ const btnFase1 = document.getElementById("fase1");
 const btnFase2 = document.getElementById("fase2");
 const btnFase3 = document.getElementById("fase3");
 
-// btnFase1.   // agregar adevenlistener
-//llamar a traerREcomendados pasandole el recomendado 1
+//Botones para cada fase
 
 btnFase1.addEventListener("click", () => {
         console.log("estoy dentro del fase 1");
@@ -99,8 +67,6 @@ btnFase3.addEventListener("click", () => {
  btnHome.addEventListener("click", ()=>{
     document.getElementById("peterpaker").style = "display:block";
     document.getElementById("contenidoCarrusel").style = "display:none";
-   // contenedorPeliculas2.innerHTML= "";
- 
  });
 
 
@@ -112,15 +78,10 @@ const traerRecomendados = (gvRecomendadosId) => {
     indicadorCarrusel.innerHTML = "";
     let lv_flag = 0;
     for (let recomendado of gvRecomendadosId) {
-        //   console.log("dentro de for " + gvRecomendadosId[i]);
-        //   console.log(recomendado);
-
+   
         fetch('http://www.omdbapi.com/?apikey=4896bf68&i=' + recomendado)
             .then(i => i.json())
             .then(i => {
-                // console.log(data.Title)
-                // console.log(data.imdbID)
-                    // console.log(i.Poster)
                     if (i.Poster == 'N/A'){
                         i.Poster = 'img/Imagen-no-disponible.png';
                     }
@@ -130,9 +91,15 @@ const traerRecomendados = (gvRecomendadosId) => {
                 <div class="item active" style="color:white"> <h1>${i.Title}</h1>
                     <img src="${i.Poster}" alt="${i.Title}" style="float:left">
                     <div>
-                        <h3> Nominaciones${i.Awards}</h3>
+                        <h3> ${i.Awards}</h3>
                         <h3> Año ${i.Year}</h3>
+                        <h3> Género ${i.Genre}</h3>
                         <h3> Ranking ${i.imdbRating}/10 </h3>
+                        <h3> Sitio Web ${i.Website}</h3>
+                        <h3> Duración ${i.Runtime}</h3>
+                        <h3> Actores ${i.Actors}</h3>
+                        <h3> ${i.Plot}</h3>
+                        
                     </div>
                     </div>
                     `
@@ -142,9 +109,14 @@ const traerRecomendados = (gvRecomendadosId) => {
                 <div class="item" style="color:white"> <h1>${i.Title}</h1>
                     <img src="${i.Poster}" alt="${i.Title}" style="float:left">
                     <div>
-                        <h3> Nominaciones${i.Awards}</h3>
-                        <h3> Año ${i.Year}</h3>
-                        <h3> Ranking ${i.imdbRating}/10 </h3>
+                    <h3> ${i.Awards}</h3>
+                    <h3> Año ${i.Year}</h3>
+                    <h3> Género ${i.Genre}</h3>
+                    <h3> Ranking ${i.imdbRating}/10 </h3>
+                    <h3> Sitio Web ${i.Website}</h3>
+                    <h3> Duración ${i.Runtime}</h3>
+                    <h3> Actores ${i.Actors}</h3>
+                    <h3> ${i.Plot}</h3>
                     </div>
                 </div>
                 `
@@ -164,50 +136,12 @@ const traerRecomendados = (gvRecomendadosId) => {
             indicadorCarrusel.innerHTML += `<br>
             <li data-target="#myCarousel" data-slide-to="${i}"></li>
         `   
-        }
-        
-
-
+        }   
     }
-
-    // for(let i = 0; i < gvRecomendadosId2.length; i++){
-    //     if (i==0 ){
-    //         indicadorCarrusel.innerHTML += `<br>
-    //         <li data-target="#myCarousel" data-slide-to="${i}" class="active"></li>
-    //     `    
-    //     }else{
-    //         indicadorCarrusel.innerHTML += `<br>
-    //         <li data-target="#myCarousel" data-slide-to="${i}"></li>
-    //     `   
-    //     }
-        
-
-
-    // }
-    // for(let i = 0; i < gvRecomendadosId3.length; i++){
-    //     if (i==0 ){
-    //         indicadorCarrusel.innerHTML += `<br>
-    //         <li data-target="#myCarousel" data-slide-to="${i}" class="active"></li>
-    //     `    
-    //     }else{
-    //         indicadorCarrusel.innerHTML += `<br>
-    //         <li data-target="#myCarousel" data-slide-to="${i}"></li>
-    //     `   
-    //     }
-        
-
-
-    // }
-    
-
 }
 
-
- 
 const appInit = ()=>{
     document.getElementById("contenidoCarrusel").style = "display:none";
-    //traerRecomendados(gvRecomendadosId3);
-
 }
 
 appInit();
